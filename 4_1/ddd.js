7c550e1b565e39b47147d43aee001c3dc4cad232
wltjr32 <fcjs9@naver.com>
2024-04-01 21:15:20 +0900

    4_1

---
+++ 4_1/ddd.js
@@ -0,0 +1,8 @@
+
+
+//elements 변수에 div 태그를 가져와서 저장했다
+const elemunts = document.querySelectorAll('div')
+//Js 사용하는 간략한 반복문
+elemunts.forEach(elemunt => {
+    console.log(element.dataset.fruitName)
+})
