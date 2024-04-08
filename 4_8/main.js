3d36e5e15a72419bb1ddeeb65fd3f74e8e789014
wltjr32 <fcjs9@naver.com>
2024-04-08 21:13:58 +0900

    4_8

---
+++ 4_8/main.js
@@ -0,0 +1,38 @@
+const myName = "jiseok"
+const email = "fcjs9@naver.com"
+const hello = `Hello ${myName}??`
+
+console.log(hello)
+
+const number = 10;
+const number2 = 10.32;
+
+const checked = True;
+const isShow = False;
+
+let undef;
+const obj = {abc:123};
+console.log(undef);
+console.log(obj.abc);
+console.log(obj.xyz);
+
+let name = null;
+console.log(name);
+name = "hihi"
+console.log(name);
+
+const user = {
+    userName : "jh",
+    age : 32,
+    isValid : true
+};
+console.log(user.userName);
+console.log(user.age);
+console.log(user.isValid);
+
+const fruits = ['apple','banana','cherry'];
+console.log(fruits[0]);
+
+var aaa = "A";
+aaa = "B"
+console.log(aaa);
