2586709e6fd631a6ccad5e8b4363eac359bdcce0
wltjr32 <fcjs9@naver.com>
2024-05-27 21:10:25 +0900

    5_27

---
+++ 5_27/app.js
@@ -0,0 +1,10 @@
+// app.js
+const http = require('http');
+
+http.createServer((requst, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type','text/plain');
+    response.end('Hello World');
+}).listen(3000);
+
+console.log('Server running at http://127.0.0.1:3000/');
