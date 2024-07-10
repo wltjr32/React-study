ab8a38ef71f3a2aba996b53641502227f4e362e5
wltjr32 <fcjs9@naver.com>
2024-07-10 19:55:27 +0900

    7_10

---
+++ shop/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <button variant='primary'>primary</button>
+    </div>
+  );
+}
+
+export default App;
