361d1d1037d753a47b2546c65d12588014173dfe
wltjr32 <fcjs9@naver.com>
2024-07-11 19:49:53 +0900

    7_11

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,69 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import data from './data.js'
+import { useState } from 'react';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import Detail from './routes/Detail.js';
 
 function App() {
+  let [shoes] = useState(data)
+  let navigate = useNavigate();
+
   return (
-    <div className="App">
-      <button variant='primary'>primary</button>
+    <div className="Main">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav ClassName="me-auto">
+            <Nav.Link onClick={()=> {navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=> {navigate('/detail')}}>Detail</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+      <Routes>
+        <Route path="/" element={
+          <>
+          <div className='main-bg'></div>
+          <div className="container">
+            <div className='row'>
+              <Card shoes={shoes[0]} i={1}></Card>
+              <Card shoes={shoes[1]} i={2}></Card>
+              <Card shoes={shoes[2]} i={3}></Card>
+            </div>
+          </div>
+          </>
+        }></Route>
+        <Route path="/detail" element={<Detail/>}></Route>
+        <Route path="*" element={<div>없는 페이지입니다 404 Error</div>}></Route>
+        <Route path="/about" element={<About/>}>
+          <Route path="member" element={<div>멤버입니다</div>}></Route>
+          <Route path="location" element={<About/>}></Route>
+          </Route>
+      </Routes>
     </div>
-  );
+  );  
 }
 
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Card(props) {
+  return (
+    <div className='col-md-4'>
+            <img src={process.env.PUBLIC_URL + '/shoe'+props.i+'.png'} width="70%"></img>
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.price}</p>
+          </div>
+  )
+}
 export default App;
+
---
+++ shop/src/data.js
@@ -0,0 +1,21 @@
+let data = [
+    {
+        id : 0,
+        title : "white and black",
+        content : "from France",
+        price : 120000
+    },
+    {
+        id : 1,
+        title : "red wine",
+        content : "from Seoul",
+        price : 110000
+    },
+    {
+        id : 2,
+        title : "Grey Jordan",
+        content : "from United States",
+        price : 130000
+    },
+]
+export default data;
